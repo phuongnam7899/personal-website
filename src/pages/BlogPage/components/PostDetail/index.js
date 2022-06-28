e70045dedaf79c0c4f9c02cbfd50ea75e1ed46b9
phuongnam7899 <phuongnam7899@gmail.com>
2022-06-28 12:05:27 +0700

    refactor

--- src/pages/BlogPage/components/PostDetail/index.js
+++ src/pages/BlogPage/components/PostDetail/index.js
@@ -7,14 +7,17 @@ import "./index.scss";
 import { IconWithTooltip, Loading } from "../../../../components";
 import { FirestoreArticle } from "../../../../apis/articles";
 import { CommentsSection } from "./components";
+import { HotPost } from "../HotPosts";
 
 export const PostDetail = () => {
   const [viewMode, setViewMode] = useLocalStorage("postViewMode", "line");
   const sliderRef = useRef(null);
   const { slug } = useParams();
   const [loading, setLoading] = useState(false);
-  const [firebaseArticle, setFirebaseArticle] = useState(new FirestoreArticle({ slug }))
-  
+  const [firebaseArticle, setFirebaseArticle] = useState(
+    new FirestoreArticle({ slug })
+  );
+
   const post = useMemo(() => {
     return posts.filter((post) => post.slug === slug)[0];
   }, [slug]);
@@ -44,16 +47,16 @@ export const PostDetail = () => {
 
   useEffect(() => {
     setLoading(true);
-    sliderRef.current && sliderRef.current.slickGoTo(0)
+    sliderRef.current && sliderRef.current.slickGoTo(0);
     firebaseArticle.updateLastestFromDbBySlug().then(() => {
-      setLoading(false)
+      setLoading(false);
       setFirebaseArticle(firebaseArticle);
     });
-  }, [slug])
+  }, [slug]);
 
   const handleAddComment = (commentContent) => {
     firebaseArticle.addComment(commentContent);
-  }
+  };
 
   const displayedPages = useMemo(() => {
     return [
@@ -75,79 +78,86 @@ export const PostDetail = () => {
 
   return (
     <div className="post-detail">
-      <div className="post-header">
-        <h2 className="post-title">{post.title.vi}</h2>
-        <div className="view-mode">
-          <div className="toggle">
-            <span
-              onClick={() => {
-                setViewMode("line");
-              }}
-              className={viewMode === "line" ? "active" : ""}
-            >
-              <IconWithTooltip
-                tooltipContent={
-                  viewMode === "card"
-                    ? "Use arrow keys (← / →) to change page"
-                    : "Read up to down like other pages"
-                }
+      <div className="article">
+        <div className="post-header">
+          <h2 className="post-title">{post.title.vi}</h2>
+          <div className="view-mode">
+            <div className="toggle">
+              <span
+                onClick={() => {
+                  setViewMode("line");
+                }}
+                className={viewMode === "line" ? "active" : ""}
               >
-                <i class="fas fa-align-left"></i>
-              </IconWithTooltip>
-            </span>
+                <IconWithTooltip
+                  tooltipContent={
+                    viewMode === "card"
+                      ? "Use arrow keys (← / →) to change page"
+                      : "Read up to down like other pages"
+                  }
+                >
+                  <i class="fas fa-align-left"></i>
+                </IconWithTooltip>
+              </span>
 
-            <span
-              className={viewMode === "card" ? "active" : ""}
-              onClick={() => {
-                setViewMode("card");
-              }}
-            >
-              <IconWithTooltip
-                tooltipContent={
-                  viewMode === "card"
-                    ? "Use arrow keys (← / →) to change page"
-                    : "Read up to down like other pages"
-                }
+              <span
+                className={viewMode === "card" ? "active" : ""}
+                onClick={() => {
+                  setViewMode("card");
+                }}
               >
-                <i class="far fa-square"></i>
-              </IconWithTooltip>
-              
-            </span>
+                <IconWithTooltip
+                  tooltipContent={
+                    viewMode === "card"
+                      ? "Use arrow keys (← / →) to change page"
+                      : "Read up to down like other pages"
+                  }
+                >
+                  <i class="far fa-square"></i>
+                </IconWithTooltip>
+              </span>
+            </div>
+            <IconWithTooltip
+              icon="question"
+              tooltipContent={
+                viewMode === "card"
+                  ? "Use arrow keys (← / →) to change page"
+                  : "Read up to down like other pages"
+              }
+            />
           </div>
-          <IconWithTooltip
-            icon="question"
-            tooltipContent={
-              viewMode === "card"
-                ? "Use arrow keys (← / →) to change page"
-                : "Read up to down like other pages"
-            }
-          />
         </div>
-      </div>
-      {viewMode === "card" ? (
-        <Slider
-          className="pages"
-          ref={(slider) => {
-            sliderRef.current = slider;
-          }}
-          {...sliderSettings}
-        >
-          {displayedPages.map((page) => {
-            return <div className="card-page">{page}</div>;
-          })}
-        </Slider>
-      ) : (
+        {viewMode === "card" ? (
+          <Slider
+            className="pages"
+            ref={(slider) => {
+              sliderRef.current = slider;
+            }}
+            {...sliderSettings}
+          >
+            {displayedPages.map((page) => {
+              return <div className="card-page">{page}</div>;
+            })}
+          </Slider>
+        ) : (
+          <div>
+            {displayedPages.map((page) => {
+              return <div className="line-page">{page}</div>;
+            })}
+          </div>
+        )}
         <div>
-          {displayedPages.map((page) => {
-            return <div className="line-page">{page}</div>;
-          })}
+          {loading ? (
+            <Loading />
+          ) : (
+            <CommentsSection
+              comments={firebaseArticle.comments}
+              addComment={handleAddComment}
+            />
+          )}
         </div>
-      )}
-      <div>
-        {
-          loading ? <Loading/> : <CommentsSection comments={firebaseArticle.comments} addComment={handleAddComment}/>
-        }
       </div>
+      <div className="right-section"><HotPost/></div>
     </div>
   );
 };
--- src/pages/BlogPage/components/Posts/index.js
+++ src/pages/BlogPage/components/Posts/index.js
@@ -1,17 +1,37 @@
 import { Masonry } from "@mui/lab";
+import { useBreakout } from "@hooks";
 import { posts } from "../../data/posts";
 import { PostPreview } from "../PostPreview";
-import './index.scss';
+import "./index.scss";
+
+const mansonrySettingByScreenType = {
+  mobile: {
+    columns: 1,
+    spacing: 1,
+  },
+  tablet: {
+    columns: 1,
+    spacing: 3,
+  },
+  desktop: {
+    columns: 2,
+    spacing: 2,
+  },
+  "desktop-l": {
+    columns: 2,
+    spacing: 3,
+  },
+};
 
 export const Posts = () => {
+  const screenType = useBreakout();
   return (
     <>
-    <div className="blog-note"><i class="fas fa-flag"></i> Note: Cause my blog's target audience now is only Vietnamese, all posts are in Vietnamese only</div>
-    <Masonry columns={2} spacing={4}>
-      {posts.map((post) => {
-        return <PostPreview post={post} />;
-      })}
-    </Masonry>
+      <Masonry {...mansonrySettingByScreenType[screenType]}>
+        {posts.map((post) => {
+          return <PostPreview post={post} />;
+        })}
+      </Masonry>
     </>
   );
 };
