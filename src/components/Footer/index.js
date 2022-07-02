9d1f626dc02d1b80b5c95223493ae52d451f650d
phuongnam7899 <phuongnam7899@gmail.com>
2022-07-02 19:27:26 +0700

    [Footer] Update version name

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,38 @@
-import './index.scss'
+import "./index.scss";
 
 export const Footer = () => {
   return (
     <footer>
       <div className="social-medias">
-        <a href='https://github.com/phuongnam7899' target='_blank' rel="noreferrer" className="media-item">
+        <a
+          href="https://github.com/phuongnam7899"
+          target="_blank"
+          rel="noreferrer"
+          className="media-item"
+        >
           <i className="fab fa-github"></i>
         </a>
-        <a href='https://www.linkedin.com/in/ph%C6%B0%C6%A1ng-nam-nguy%E1%BB%85n-96740317b' rel="noreferrer" target='_blank' className="media-item">
+        <a
+          href="https://www.linkedin.com/in/ph%C6%B0%C6%A1ng-nam-nguy%E1%BB%85n-96740317b"
+          rel="noreferrer"
+          target="_blank"
+          className="media-item"
+        >
           <i className="fab fa-linkedin"></i>
         </a>
-        <a href='https://www.instagram.com/namkhonglanman/' rel="noreferrer" target='_blank' className="media-item">
-        <i className="fab fa-instagram-square"></i>
+        <a
+          href="https://www.instagram.com/namkhonglanman/"
+          rel="noreferrer"
+          target="_blank"
+          className="media-item"
+        >
+          <i className="fab fa-instagram-square"></i>
         </a>
       </div>
-      <div className='copyright'><i className="far fa-copyright"></i> 2022 nklm <span className='version'>v1.0.0</span></div>
+      <div className="copyright">
+        <i className="far fa-copyright"></i> 2022 nklm{" "}
+        <span className="version">v1.1.0</span>
+      </div>
     </footer>
   );
 };
